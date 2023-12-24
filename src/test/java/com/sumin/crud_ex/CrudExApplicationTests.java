3146238ea7fccf20fa2cd432d813ce3614b97e27
suminpark123 <a55026851@gmail.com>
2023-12-25 07:45:53 +0900

    commit

---
+++ src/test/java/com/sumin/crud_ex/CrudExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumin.crud_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
